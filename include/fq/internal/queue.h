6d58e0a2f8060e1d9e35ca64b44308fed0f13c19
~mario rosell <hyper1516gamer@gmail.com>
2025-12-28 15:25:18 +0100

    queue: add queue header

---
+++ include/fq/internal/queue.h
@@ -0,0 +1,23 @@
+#ifndef _FQ_INTERNAL_QUEUE_H_
+#define _FQ_INTERNAL_QUEUE_H_	1
+
+#include "../fault.h"
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
+
+#define MAXFAULTS	16
+
+typedef struct queue {
+	Fault* items[MAXFAULTS];
+	int8_t front, rear;
+} Queue;
+
+#ifdef __cplusplus
+	}
+#endif
+
+#endif /* _FQ_INTERNAL_QUEUE_H_ */
